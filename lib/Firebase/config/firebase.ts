1164c6a222a8629032184e3b69f341d4b1b9b3a0
kabuki <tanuki5sanuki3@icloud.com>
2022-12-08 10:38:33 +0900

    add env

--- lib/Firebase/config/firebase.ts
+++ lib/Firebase/config/firebase.ts
@@ -4,16 +4,16 @@ import { getAuth } from 'firebase/auth'
 import { getAnalytics } from 'firebase/analytics';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
-import { getFunctions } from 'firebase/functions'
+
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA5V464iLv4yopbTXOZSJ7fCldCQUlLzQE",
-    authDomain: "chat-ai-5778d.firebaseapp.com",
-    projectId: "chat-ai-5778d",
-    storageBucket: "chat-ai-5778d.appspot.com",
-    messagingSenderId: "449303245488",
-    appId: "1:449303245488:web:078a2256e2b9a79f554175",
-    measurementId: "G-199SSTDT8F"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 }
 
 export const app = initializeApp(firebaseConfig)
